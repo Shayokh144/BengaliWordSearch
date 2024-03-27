da337f144bfe2158e404ccbda96bddb2feb12959
Shayokh144 <shayokhbd144@gmail.com>
2024-03-27 11:22:17 +0700

    Add copy text feature

--- app/BengaliWordSearch/BengaliWordSearch/HomeScreen.swift
+++ app/BengaliWordSearch/BengaliWordSearch/HomeScreen.swift
@@ -5,64 +5,111 @@
 //  Created by Taher's nimble macbook on 25/3/2567 BE.
 //
 
+import Combine
 import SwiftUI
 
 struct HomeScreen: View {
     
     @StateObject private var viewModel: HomeViewModel
+    @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var copiedWord: String = ""
     
-    var suffixView: some View {
+    private var suffixView: some View {
         ScrollView(showsIndicators: false) {
             LazyVStack(alignment: .leading) {
                 ForEach(viewModel.suffixMatchWords, id: \.self) { word in
                     Text(word)
                         .padding(.bottom, 2.0)
+                        .onTapGesture(count: 2) {
+                            processCopyText(word: word)
+                        }
                 }
             }
         }
     }
     
-    var prefixView: some View {
+    private var prefixView: some View {
         ScrollView(showsIndicators: false) {
             LazyVStack(alignment: .leading) {
                 ForEach(viewModel.prefixMatchWords, id: \.self) { word in
                     Text(word)
                         .padding(.bottom, 2.0)
+                        .onTapGesture(count: 2) {
+                           processCopyText(word: word)
+                        }
                 }
             }
         }
     }
     
+    private var copyTextToast: some View {
+        Text("\(copiedWord) copied to clipboard")
+            .foregroundStyle(Color.black)
+            .padding(8.0)
+            .frame(maxWidth: .infinity)
+            .background(Color.gray.opacity(0.7))
+            .cornerRadius(16.0)
+    }
+    
     var body: some View {
         NavigationStack {
-            switch viewModel.state {
-                case .loading:
-                    ProgressView()
-                case .loaded:
-                    HStack {
-                        prefixView
-                        Spacer()
-                        suffixView
-                    }
-                    .padding()
-                case .error:
-                    Text("Error")
-                    Button("Try again", action: viewModel.tryAgain)
-                    
+            VStack {
+                switch viewModel.state {
+                    case .loading:
+                        ProgressView()
+                    case .loaded:
+                        VStack {
+                            if !copiedWord.isEmpty {
+                                copyTextToast
+                            }
+                            HStack {
+                                prefixView
+                                Spacer()
+                                suffixView
+                            }
+                            .padding(.top, 8.0)
+                        }
+                        .padding()
+                        .animation(.linear, value: copiedWord.isEmpty)
+                    case .error:
+                        Text("Error")
+                        Button("Try again", action: viewModel.tryAgain)
+                        
+                }
             }
+            .navigationTitle("Search Bengali Word")
+            .searchable(
+                text: $viewModel.searchText,
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "write here"
+            )
+            .padding()
         }
-        .searchable(
-            text: $viewModel.searchText,
-            placement: .navigationBarDrawer(displayMode: .always),
-            prompt: "write here"
-        )
-        .padding()
         .onAppear {
             viewModel.viewAppear()
+            stopTimer()
+        }
+        .onReceive(timer) { value in
+            copiedWord = ""
+            stopTimer()
         }
     }
     
     init(viewModel: HomeViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
+    }
+    
+    func stopTimer() {
+        timer.upstream.connect().cancel()
+    }
+    
+    func startTimer() {
+        timer = Timer.publish(every: 2.0, on: .main, in: .common).autoconnect()
+    }
+    
+    private func processCopyText(word: String) {
+        UIPasteboard.general.string = word
+        copiedWord = word
+        startTimer()
     }
 }
